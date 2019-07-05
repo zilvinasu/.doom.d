9ca1cb15b34b39e722e9467737aadff32ff3a2a2
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2019-07-05 12:37:09 +0300

    feat(config): add binding for +ivy/project-search

--- config.el
+++ config.el
@@ -10,3 +10,9 @@
 (eval-after-load 'flycheck '(flycheck-clojure-setup))
 (eval-after-load 'flycheck
   '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+
+;; Custom Key Bindings
+(map!
+ :leader
+ (:prefix "p"
+  :desc "Search" "s" #'+ivy/project-search))
