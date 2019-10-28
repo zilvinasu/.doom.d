18027de9927d70a99a5e067c2df956941020ba7d
Zilvinas Urbonas <zilvinas.urbon@gmail.com>
2019-10-28 12:07:20 +0200

    feat(config): default light theme

--- config.el
+++ config.el
@@ -1,7 +1,7 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-one-light)
 (setq display-line-numbers-type nil)
 (menu-bar-mode 1)
 (global-visual-line-mode 1)
