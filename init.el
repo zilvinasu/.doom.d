2ef75718775ee42a554c1c286fd0834917eb4b79
Zilvinas Urbonas <zilvinas.urbon@gmail.com>
2020-07-14 09:12:09 +0300

    feat(init): add yaml mode

--- init.el
+++ init.el
@@ -154,7 +154,7 @@
        swift               ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web                 ; the tubes
-       ;;vala              ; GObjective-C
+       yaml                ; JSON, but readable
 
        :email
        ;;(mu4e +gmail)       ; WIP
